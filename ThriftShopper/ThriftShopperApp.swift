9773493b37d57569455192961997fde13b7e7672
Dominic Ndondo <59946287+Dominic263@users.noreply.github.com>
2024-02-08 16:20:45 -0500

    Project init. Login and Registration flows.

---
+++ ThriftShopper/ThriftShopperApp.swift
@@ -0,0 +1,18 @@
+//
+//  ThriftShopperApp.swift
+//  ThriftShopper
+//
+//  Created by DOMINIC NDONDO on 2/8/24.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+@main
+struct ShopperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
