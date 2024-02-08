6ede2a0ef05f5429680d2360789dc393182a6bd0
Dominic Ndondo <59946287+Dominic263@users.noreply.github.com>
2024-02-08 16:25:58 -0500

    Package shopper-ios-client added to root project.

---
+++ ThriftShopper/ThriftShopper.swift
@@ -0,0 +1,17 @@
+//
+//  ThiftShopperApp.swift
+//  ThiftShopper
+//
+//  Created by DOMINIC NDONDO on 2/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ThiftShopperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
